775c1f1719af9cb75046c643317abcfec3194c13
Ed Wellbrook <edwellbrook@gmail.com>
2016-06-19 16:22:05 +0100

    Prepare for Swift Package Manager

---
+++ Package.swift
@@ -0,0 +1,13 @@
+//
+//  Package.swift
+//  GoSquaredAPI
+//
+//  Created by Edward Wellbrook on 19/06/2016.
+//  Copyright © 2016 Go Squared Ltd. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "GoSquaredAPI"
+)
--- Sources/GoSquaredAPI.h
+++ Sources/GoSquaredAPI.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015 Go Squared Ltd. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for GoSquaredAPI.
 FOUNDATION_EXPORT double GoSquaredAPIVersionNumber;
